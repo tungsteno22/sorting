ec00652c8454228f0906f40060d5231601917ac6
Nathan Avilez <tungsteno22@gmail.com>
2018-09-17 11:40:33 -0500

    it is working

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,21 @@
+function swapper(val, comparator) {
+  if (val > comparator) return true;
+  return false;
+}
+
 function bubbleSort(array) {
-  /* your code here */
+  let totalLoops = array.length - 1;
+  while (totalLoops > 0) {
+    for (let i = 0; i < totalLoops; ++i) {
+      let currIdx = array[i];
+      let nextIdx = array[i + 1];
+
+      if (swapper(currIdx, nextIdx)) {
+        array[i] = nextIdx;
+        array[i + 1] = currIdx;
+      }
+    }
+    --totalLoops;
+  }
+  return array;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,4 +2,10 @@ describe('Bubble Sort', function() {
   it('handles an empty array', function() {
     expect(bubbleSort([])).toEqual([]);
   });
+
+  it('Compares values the correct number of times', function() {
+    spyOn(window, 'swapper').and.callThrough();
+    window.bubbleSort([3, 2, 4]);
+    expect(window.swapper.calls.count()).toEqual(3);
+  });
 });
